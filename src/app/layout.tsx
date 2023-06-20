d310f673d7e33c2f4a4596187e0e27badc88c68b
Henrique Moreno <henriqueprofissional26@gmail.com>
2023-06-19 22:22:15 -0300

    add files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { ClerkProvider } from '@clerk/nextjs'
+import { ptBR } from '@clerk/localizations'
+import { neobrutalism } from '@clerk/themes'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,8 +17,21 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider localization={ptBR}
+      appearance={{
+        baseTheme: neobrutalism,
+        elements: {
+          formButtonPrimary: 'bg-violet-500 hover:bg-violet-600'
+        }
+      }}
+    >
+      <html lang="pt-br">
+        <body className={inter.className}>
+          <div className='h-screen flex justify-center items-center'>
+            {children}
+          </div>
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
